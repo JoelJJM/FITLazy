d590b8e672147f536924b8fd860a81e58c86c3cd
Joel Morgan <joeljmorgan@icloud.com>
2024-11-18 21:01:29 +0000

    Building basic sign-in form

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,28 +1,46 @@
-"use client";
-
-import { useState } from "react";
-import { sculptureList } from "./data.js";
-
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-
-  function handleClick() {
-    setIndex(index + 1);
-  }
-
-  let sculpture = sculptureList[index];
+export default function Signup() {
   return (
-    <>
-      <button onClick={handleClick}>Next</button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <img src={sculpture.url} alt={sculpture.alt} />
-      <p>{sculpture.description}</p>
-    </>
+    <div className="flex w-screen h-screen justify-center items-center">
+      <form>
+        <div className="flex flex-col gap-10 border-2 items-center border-black rounded-xl p-5">
+          <div className="flex flex-col items-center">
+            <h1 className="text-2xl font-bold">Signup</h1>
+            <h2 className="text-md">Sign up today and be fitLAZY</h2>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              className="border-[1px] border-black p-3 rounded-xl text-md"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              className="border-[1px] border-black p-3 rounded-xl text-md"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password1"
+              className="border-[1px] border-black p-3 rounded-xl text-md"
+            />
+            <input
+              type="password"
+              placeholder="Re-type Password"
+              name="password2"
+              className="border-[1px] border-black p-3 rounded-xl text-md"
+            />
+          </div>
+          <button
+            type="submit"
+            className="p-3 text-white bg-gray-700 rounded-xl w-full hover:opacity-85 active:opacity-75"
+          >
+            Confirm
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
